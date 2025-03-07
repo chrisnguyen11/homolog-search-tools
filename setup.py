64430f585bbd04ec483b352721e2bdb74de30b9b
Chris Nguyen <cnguyen11@luc.com>
2025-03-07 17:03:45 -0600

    Add python setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='homolog_search_tools',
+   version='0.1',
+   description='Module for streamlining homolog search process.',
+   author='Chris Nguyen',
+   author_email='cnguyen11@luc.edu',
+   packages=['homolog_search_tools']
+)
