9b59357e468e1c6ab57a0d239b534029fb554079
Chris Nguyen <cnguyen11@luc.com>
2025-03-08 00:03:23 -0600

    Fix import

--- homolog_search_tools/__init__.py
+++ homolog_search_tools/__init__.py
@@ -1,11 +1,15 @@
 """Importing submodules."""
 
-from . import search
-from . import similarity
-from . import utils
+# from . import search
+# from . import similarity
+# from . import utils
 
-__all__ = [
-    "search",
-    "similarity",
-    "utils"
-]
+# __all__ = [
+#     "search",
+#     "similarity",
+#     "utils"
+# ]
+
+from .search import UniProtRequest, uniprotrecords_to_dataframe
+from .similarity import MMseqs2
+from .utils import read_fasta, write_fasta
