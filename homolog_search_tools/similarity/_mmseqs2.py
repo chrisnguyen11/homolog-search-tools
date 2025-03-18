cb3cf551bd833ee61d58cd09faa53319d9e1ff52
Chris Nguyen <cnguyen11@luc.com>
2025-03-18 18:52:18 -0500

    Add type hinit

--- homolog_search_tools/similarity/_mmseqs2.py
+++ homolog_search_tools/similarity/_mmseqs2.py
@@ -3,27 +3,8 @@
 import tempfile
 import os
 import pandas as pd
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD:homolog_search_tools/similarity/_mmseqs2.py
-<<<<<<< HEAD:homolog_search_tools/similarity/_mmseqs2.py
-from ..utils._utils import cmd_run, handle_sequence_data
+from ..utils._utils import SequenceData, cmd_run, handle_sequence_data
 from ._similarity_utils import read_tblastout
-=======
-from .utils.utils import cmd_run, handle_sequence_data
-=======
-from ..utils.utils import cmd_run, handle_sequence_data
->>>>>>> 9ed310e (Fix import):homolog_search_tools/similarity/mmseqs2.py
-from .similarity_utils import read_tblastout
->>>>>>> 6b4b546 (Fix import):homolog_search_tools/similarity/mmseqs2.py
-=======
-from ..utils._utils import cmd_run, handle_sequence_data
-from ._similarity_utils import read_tblastout
->>>>>>> 489da92 (Fix import)
-=======
-from ..utils._utils import cmd_run, handle_sequence_data
-from ._similarity_utils import read_tblastout
->>>>>>> 8e7514b6915c0b1501799d517b9e669392c3594f
 
 class MMseqs2:
     """Class to interact with MMseqs."""
@@ -31,7 +12,7 @@ class MMseqs2:
     def __init__(self, path_to_binary='mmseqs'):
         self.path_to_binary = path_to_binary
 
-    def run(self, query_sequences, target_sequences) -> pd.DataFrame:
+    def run(self, query_sequences:SequenceData, target_sequences:SequenceData) -> pd.DataFrame:
         """
         Command wrapper for MMseqs2 to cluster a database, 
         or do an all-against-all pairwise alignment.
@@ -68,7 +49,7 @@ class MMseqs2:
             df = read_tblastout(output_file)
         return df
 
-    def run_allvsall(self, sequences) -> pd.DataFrame:
+    def run_allvsall(self, sequences:SequenceData) -> pd.DataFrame:
         """
         Equivalent to MMseqs.run(query_sequences=sequences, target_sequences=sequences).
         """
