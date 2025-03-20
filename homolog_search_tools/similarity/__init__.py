7f1fcea350dff900bd44e7e388427c5ace6097f3
Chris Nguyen <cnguyen11@luc.com>
2025-03-20 16:30:50 -0500

    Add blastp functionality

--- homolog_search_tools/similarity/__init__.py
+++ homolog_search_tools/similarity/__init__.py
@@ -1,9 +1,11 @@
 """Sequence similarity tools."""
 
-from ._mmseqs2 import MMseqs2
+from ._blastp import BlastP
 from ._diamond import Diamond
+from ._mmseqs2 import MMseqs2
 
 __all__ = [
+    "BlastP",
+    "Diamond",
     "MMseqs2",
-    "Diamond"
 ]
