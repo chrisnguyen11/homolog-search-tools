d55a5f2c6681392ed8cd6f997608fe5918ee2952
Chris Nguyen <cnguyen11@luc.com>
2025-03-20 17:16:27 -0500

    Fix _alphabetized_accessions

--- homolog_search_tools/similarity/_similarity_utils.py
+++ homolog_search_tools/similarity/_similarity_utils.py
@@ -20,12 +20,14 @@ def _compute_log_evalue(evalues):
     max_log_evalue = log_evalue[~np.isinf(log_evalue)].max()
     return np.where(np.isinf(log_evalue), max_log_evalue, log_evalue)
 
-def _alphabetized_accessions(query_accession, target_accession):
+def _alphabetized_accessions(accessions):
     """
     For a tuple (pair) of query and target accesssions, return alphabetized tuple.
     Assumes (query_accession, target_accession) == (target_accession, query_accession),
         order does not matter.
     """
+    query_accession = accessions["Query_Accession"]
+    target_accession = accessions["Target_Accession"]
     _min = min(query_accession, target_accession)
     _max = max(query_accession, target_accession)
     return (_min, _max)
@@ -49,10 +51,10 @@ def _read_tblastout(path_or_buff, sep:str="\t") -> pd.DataFrame:
         "E_Value", "Bit_Score"
     ]
     tblast_columns_float = [
-        "Percent_Identity","E_Value", "Bit_Score"
+        "Percent_Identity", "E_Value", "Bit_Score"
     ]
     tblast_columns_int = [
-        "Alignment_Length","Mismatches", "Gap_Openings", "Query_Start", "Query_End", 
+        "Alignment_Length", "Mismatches", "Gap_Openings", "Query_Start", "Query_End", 
         "Target_Start", "Target_End"
     ]
     df =  pd.read_csv(path_or_buff, sep=sep, names=tblast_columns)
@@ -65,11 +67,12 @@ def read_transform_tblastout(path_or_buff, sep:str="\t") -> pd.DataFrame:
     Read and transform tblastout.
     """
     final_columns = [
-        "Accession_1", "Accession_2""Percent_Identity", "Alignment_Length",
+        "Accession_1", "Accession_2", "Percent_Identity", "Alignment_Length",
         "Mismatches", "Gap_Openings", "Query_Start", "Query_End", "Target_Start", "Target_End",
         "E_Value", "Bit_Score"
     ]
     df = _read_tblastout(path_or_buff, sep)
     df["Log_E_Value"] = _compute_log_evalue(df["E_Value"])
-    df[["Accession_1", "Accession_2"]] = df[["Query_Accession", "Target_Accession"]].apply(_alphabetized_accessions, axis=1)
+    df[["Accession_1", "Accession_2"]] = df[["Query_Accession", "Target_Accession"]].apply(
+        _alphabetized_accessions, axis=1).to_list()
     return df.sort_values("Log_E_Value", ascending=False)[final_columns]
