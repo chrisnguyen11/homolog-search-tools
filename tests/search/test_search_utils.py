97de8543851b63b4a5aa96892b45fc547f9a7886
Chris Nguyen <cnguyen11@luc.com>
2025-10-17 15:50:53 -0500

    Update test for batch_request

--- tests/search/test_search_utils.py
+++ tests/search/test_search_utils.py
@@ -1,7 +1,18 @@
 from homolog_search_tools.search._search_utils import batch_request
 
+def peudo_request_func(array):
+    """
+    Given an array of integers, 
+    Throw an error if array contains any integers divisible by 9 and
+    Return an array of integers not divisible by 9.
+    """
+    output = []
+    for x in array:
+        if x % 10 == 0:
+            raise Exception()
+        output.append(x)
+    return output
+
 def test_batch_request():
-    def example_func(x):
-        return [sum(x)]
-   
-    assert batch_request(example_func, list(range(10)), chunk_size=2) == [1, 5, 9, 13, 17]
+    output = [x for x in range(100) if x % 10 != 0]
+    assert batch_request(peudo_request_func, list(range(100))) == output
