ad47eefcf8d31f95ee78cb38f83ec07755f0a62c
Chris Nguyen <cnguyen11@luc.com>
2025-10-17 15:32:48 -0500

    Add recursion to batch request to handle errors

--- homolog_search_tools/search/_search_utils.py
+++ homolog_search_tools/search/_search_utils.py
@@ -9,44 +9,55 @@ UniProtRecord = Dict
 
 UniProtRequestFields = [
     # Names & Taxonomy
-    'accession', 'id', 'gene_names', 'gene_primary', 'gene_synonym',
-    'organism_name', 'organism_id', 'protein_name',
+    "accession", "id", "gene_names", "gene_primary", "gene_synonym",
+    "organism_name", "organism_id", "protein_name",
     # Sequences
-    'organelle', 'length', 'mass', 'sequence', 'sequence_version',
+    "organelle", "length", "mass", "sequence", "sequence_version",
     # Function
-    'ft_act_site', 'ft_binding', 'cc_catalytic_activity', 'cc_cofactor',
-    'ec', 'rhea', 'temp_dependence',
+    "ft_act_site", "ft_binding", "cc_catalytic_activity", "cc_cofactor",
+    "ec", "rhea", "temp_dependence",
     # Miscellaneous
-    'annotation_score', 'protein_existence', 'reviewed', 'uniparc_id',
+    "annotation_score", "protein_existence", "reviewed", "uniparc_id",
     # Interaction
-    'cc_interaction', 'cc_subunit',
+    "cc_interaction", "cc_subunit",
     # Gene Ontology (GO)
-    'go',
+    "go",
     # Subcellular location
-    'cc_subcellular_location', 'ft_transmem',
+    "cc_subcellular_location", "ft_transmem",
     # Structure
-    'structure_3d',
+    "structure_3d",
     # Family and domain
-    'xref_cdd', 'xref_disprot', 'xref_gene3d', 'xref_hamap', 'xref_interpro',
-    'xref_ncbifam', 'xref_panther', 'xref_pfam', 'xref_pirsf', 'xref_prints',
-    'xref_prosite', 'xref_sfld', 'xref_smart', 'xref_supfam'
+    "xref_cdd", "xref_disprot", "xref_gene3d", "xref_hamap", "xref_interpro",
+    "xref_ncbifam", "xref_panther", "xref_pfam", "xref_pirsf", "xref_prints",
+    "xref_prosite", "xref_sfld", "xref_smart", "xref_supfam"
 ]
 
-def batch_request(request_func:Callable, accession, chunk_size:int=500, **kwarg):
+def batch_request(request_func:Callable, accessions:Accession, batch_size:int=500, **kwarg):
     """
-    Batch run the request_func.
+    Exectues request_func in batches. Can handle potential
+    unknown accession (404) errors.
 
     Parameters
     ----------
-    - request_func: Callable: request function to run in batches.
-    - chunk_size: int: batch size. Default: 500.
+    - request_func: Callable: api request function to execute in batches.
+    - accession: list of accessions
+    - batch_size: int: batch size. Default: 500.
 
     Reqturns
     --------
     : : concatenated output of the request function.
     """
-    out = []
-    for i in range(0, len(accession), chunk_size):
-        chunk = accession[i:i+chunk_size]
-        out.extend(request_func(chunk, **kwarg))
-    return out
+    output = []
+    if len(accessions) == 1:
+        try:
+            output.extend(request_func(accessions))
+        except:
+            print(f"Unable to handle {accessions}")
+    else:
+        for i in range(0, len(accessions), batch_size):
+            batch = accessions[i: i+batch_size]
+            try:
+                output.extend(request_func(batch))
+            except:
+                output.extend(batch_request(request_func, batch, batch_size//2))
+    return output
